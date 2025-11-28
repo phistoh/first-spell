7f4ea6566d351655573d8ecebb037debab2c7da5
phistoh <phis@mogobb.de>
2025-11-28 23:56:00 +0100

    renamed drgaonfly

--- kubejs/client_scripts/renamed_items.js
+++ kubejs/client_scripts/renamed_items.js
@@ -1,8 +1,10 @@
 ClientEvents.lang("en_us", (event) => {
   event.renameItem("supplementaries:antique_ink", "Antique™ Ink");
   event.renameItem("crittersandcompanions:grappling_hook", "Extra Sticky Silk");
+  event.renameItem("moresnifferflowers:dragonfly", "Toy Dragonfly");
 });
 
 ItemEvents.modifyTooltips(event => {
     event.add('supplementaries:antique_ink', 'Quill included.')
+    event.add('moresnifferflowers:dragonfly', 'Now with free levitation.')
 });
