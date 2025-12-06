b6cf1564b21e9a82eee9ae9e605ae15daebc6e70
phistoh <phis@mogobb.de>
2025-12-06 23:19:39 +0100

    now with mana conservation skill

--- kubejs/server_scripts/skills/class_based/mana_regen.js
+++ kubejs/server_scripts/skills/class_based/mana_regen.js
@@ -1,3 +1,8 @@
-// PlayerEvents.spellOnCast((event) => {
-//   console.log(event);
-// });
+ISSEvents.spellOnCast((event) => {
+  const start_index = String(event.schoolType.id).indexOf(":") + 1;
+  const iss_school = String(event.schoolType.id).substring(start_index);
+  if (!event.entity.tags.contains(`phis.conserve_mana_${iss_school}`)) return;
+  if (Math.random() < 0.1) {
+    event.manaCost = 0;
+  }
+});
