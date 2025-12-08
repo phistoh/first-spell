8385c76e18c63aaad4ec8fd3c33a334f0442b6f4
phistoh <phis@mogobb.de>
2025-12-08 20:54:43 +0100

    spawn eggs

--- kubejs/server_scripts/skills/spawn_eggs.js
+++ kubejs/server_scripts/skills/spawn_eggs.js
@@ -4,23 +4,21 @@ EntityEvents.death((event) => {
     source: { player },
   } = event;
 
-  const spawn_egg_table = {
-    "minecraft:chicken": "minecraft:chicken_spawn_egg",
-  };
-
   if (!player) return;
   if (entity.level.clientSide) return;
+  if (!entity.living) return;
   if (!player.tags.contains("phis.spawn_eggs")) return;
 
   if (entity.lastHurtByPlayer) {
     const mob_id = entity.type;
-    if (mob_id in spawn_egg_table && Math.random() < 0.01) {
+    if (mob_id == "minecraft:player") return;
+    if (Math.random() < 0.01) {
       player.runCommandSilent(
-        `summon item ${entity.x} ${entity.y} ${
-          entity.z
-        } {Motion:[${Math.random()/8}d,${Math.random()/8}d,${Math.random()/8}d], Item:{id:"${
-          spawn_egg_table[mob_id]
-        }",count:1}}`
+        `summon item ${entity.x} ${entity.y} ${entity.z} {Motion:[${
+          Math.random() / 8
+        }d,${Math.random() / 8}d,${
+          Math.random() / 8
+        }d], Item:{id:"${mob_id}_spawn_egg",count:1}}`
       );
     }
   }
