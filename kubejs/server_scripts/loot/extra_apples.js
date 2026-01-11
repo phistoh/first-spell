9cb702ecbac37a64f2190fea6fb115722afebea7
phistoh <phis@mogobb.de>
2026-01-11 12:12:10 +0100

    fixed extra apples loot

--- kubejs/server_scripts/loot/extra_apples.js
+++ kubejs/server_scripts/loot/extra_apples.js
@@ -1,21 +1,33 @@
 const entry = LootEntry.alternative(
-  LootEntry.of("minecraft:golden_apple").when((c) => c.randomChance(0.01)),
+  LootEntry.of("minecraft:golden_apple").when(
+    (c) =>
+      c.randomChance(0.1) &&
+      c.matchMainHand(
+        ItemFilter.anyOf(
+          ItemFilter.hasEnchantment("minecraft:silk_touch"),
+          ItemFilter.tag("#c:tools/shear"),
+          ItemFilter.tag("#c:tools/shears")
+        ).negate()
+      ) &&
+      c.matchPlayerCustom((player) => {
+        return player && player.tags.contains("phis.extraapples");
+      })
+  ),
   LootEntry.of("minecraft:enchanted_golden_apple").when((c) =>
-    c.randomChance(0.001)
+    c.randomChance(0.001) &&
+      c.matchMainHand(
+        ItemFilter.anyOf(
+          ItemFilter.hasEnchantment("minecraft:silk_touch"),
+          ItemFilter.tag("#c:tools/shear"),
+          ItemFilter.tag("#c:tools/shears")
+        ).negate()
+      ) &&
+      c.matchPlayerCustom((player) => {
+        return player && player.tags.contains("phis.extraapples");
+      })
   ),
   LootEntry.empty()
-).when((condition) => {
-  condition.matchMainHand(
-    ItemFilter.anyOf(
-      ItemFilter.hasEnchantment("minecraft:silk_touch"),
-      ItemFilter.tag("#c:tools/shear"),
-      ItemFilter.tag("#c:tools/shears")
-    ).negate()
-  ) &&
-    condition.matchPlayerCustom((player) => {
-      return player && player.tags.contains("phis.extraapples");
-    });
-});
+);
 
 LootJS.modifiers((event) => {
   event.addBlockModifier("#minecraft:leaves").addLoot(entry);
