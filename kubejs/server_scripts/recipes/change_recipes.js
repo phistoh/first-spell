5749c559743847707498c48f05fd7a02a1a114df
phistoh <phis@mogobb.de>
2026-01-14 20:00:42 +0100

    changed recipe for Lava Fishing Rod

--- kubejs/server_scripts/recipes/change_recipes.js
+++ kubejs/server_scripts/recipes/change_recipes.js
@@ -1,1 +1,4 @@
-
+ServerEvents.recipes((event) => {
+  event.remove({ id: "netherdepthsupgrade:lava_fishing_rod" });
+  event.shapeless("netherdepthsupgrade:lava_fishing_rod", ["minecraft:fishing_rod", "minecraft:potion[potion_contents={potion:\"fire_resistance\"}]"]);
+})
