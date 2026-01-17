01d60fa1b92c3ab70de45bff7b1f67ff71e5817e
phistoh <phis@mogobb.de>
2026-01-17 21:28:16 +0100

    make manuscripts lootable

---
+++ kubejs/server_scripts/loot/manuscripts.js
@@ -0,0 +1,112 @@
+LootJS.lootTables((event) => {
+  const schools = [
+    "familiarslib:sound",
+    "irons_spellbooks:fire",
+    "irons_spellbooks:ice",
+    "irons_spellbooks:lightning",
+    "irons_spellbooks:holy",
+    "irons_spellbooks:ender",
+    "irons_spellbooks:blood",
+    "irons_spellbooks:evocation",
+    "irons_spellbooks:nature",
+  ];
+  event.create("phis:manuscripts").createPool((pool) => {
+    for (const school of schools) {
+      pool.addEntry(
+        LootEntry.of(
+          `irons_restrictions:manuscript[irons_restrictions:school_component="${school}"]`,
+        ),
+      );
+    }
+    pool.rolls([0, 1]);
+  });
+});
+
+LootJS.modifiers((event) => {
+  event.addTableModifier("minecraft:chests/simple_dungeon").pool((pool) => {
+    pool.addEntry(LootEntry.reference("phis:manuscripts").randomChance(0.1));
+  });
+});
+
+LootJS.lootTables((event) => {
+  const skeletons = [
+    "realmrpg_skeletons:acrobat_skeleton",
+    "realmrpg_skeletons:arrow_skeleton",
+    "realmrpg_skeletons:basic_skeleton",
+    "realmrpg_skeletons:bow_skeleton",
+    "realmrpg_skeletons:buried_skeleton",
+    "realmrpg_skeletons:burnt_skeleton",
+    "realmrpg_skeletons:cactus_skeleton",
+    "realmrpg_skeletons:chorus_skeleton",
+    "realmrpg_skeletons:chorus_tangled_skeleton",
+    "realmrpg_skeletons:common_skeleton",
+    "realmrpg_skeletons:corrupted_skeleton",
+    "realmrpg_skeletons:crimson_tangled_skeleton",
+    "realmrpg_skeletons:crystal_skeleton",
+    "realmrpg_skeletons:devastated_skeleton",
+    "realmrpg_skeletons:dragon_burnt_skeleton",
+    "realmrpg_skeletons:dripstone_skeleton",
+    "realmrpg_skeletons:duelist_skeleton",
+    "realmrpg_skeletons:dungeon_crawler_skeleton",
+    "realmrpg_skeletons:dusty_skeleton",
+    "realmrpg_skeletons:explorer_skeleton",
+    "realmrpg_skeletons:frozen_waterdrop_skeleton",
+    "realmrpg_skeletons:fungus_gatherer_skeleton",
+    "realmrpg_skeletons:headache_skeleton",
+    "realmrpg_skeletons:headless_skeleton",
+    "realmrpg_skeletons:hunted_skeleton",
+    "realmrpg_skeletons:lucky_skeleton",
+    "realmrpg_skeletons:melted_skeleton",
+    "realmrpg_skeletons:mossy_skeleton",
+    "realmrpg_skeletons:mushroomer_skeleton",
+    "realmrpg_skeletons:neutralized_skeleton",
+    "realmrpg_skeletons:pierced_skeleton",
+    "realmrpg_skeletons:powder_snow_skeleton",
+    "realmrpg_skeletons:quicksand_skeleton",
+    "realmrpg_skeletons:rookie_skeleton",
+    "realmrpg_skeletons:shot_skeleton",
+    "realmrpg_skeletons:shroom_skeleton",
+    "realmrpg_skeletons:skeleton_from_the_sky",
+    "realmrpg_skeletons:skeleton_no_spawn",
+    "realmrpg_skeletons:slain_skeleton",
+    "realmrpg_skeletons:snow_skeleton",
+    "realmrpg_skeletons:spider_victim_skeleton",
+    "realmrpg_skeletons:stalactite_skeleton",
+    "realmrpg_skeletons:swimmer_skeleton",
+    "realmrpg_skeletons:thief_skeleton",
+    "realmrpg_skeletons:trident_skeleton",
+    "realmrpg_skeletons:unsaved_skeleton",
+    "realmrpg_skeletons:vines_tangled_skeleton",
+    "realmrpg_skeletons:waterdrop_skeleton",
+    "realmrpg_skeletons:webbed_skeleton",
+    "realmrpg_skeletons:winter_skeleton",
+    "realmrpg_skeletons:wither_skeleton_no_spawn",
+  ];
+  for (const skeleton of skeletons) {
+    event.modifyBlockTables(skeleton).createPool((pool) => {
+      pool
+        .addEntry(LootEntry.reference("phis:manuscripts").randomChance(0.1))
+        .when((condition) => {
+          condition.matchMainHand(
+            ItemFilter.hasEnchantment("minecraft:silk_touch").negate(),
+          );
+        });
+    });
+  }
+});
+
+LootJS.modifiers((event) => {
+  const magic_mobs = [
+    "minecraft:entities/illusioner",
+    "minecraft:entities/witch",
+    "irons_spellbooks:entities/archevoker",
+    "irons_spellbooks:entities/cryomancer",
+    "irons_spellbooks:entities/necromancer",
+    "irons_spellbooks:entities/pyromancer",
+  ];
+  for (const magic_mob of magic_mobs) {
+    event.addTableModifier(magic_mobs).pool((pool) => {
+      pool.addEntry(LootEntry.reference("phis:manuscripts").randomChance(0.1));
+    });
+  }
+});
