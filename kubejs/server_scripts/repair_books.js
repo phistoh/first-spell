d5cea566f121e783d8c2c462b8dee2c4b7ce2c2f
phistoh <phis@mogobb.de>
2026-01-18 01:03:31 +0100

    fixed book repair script to work on damaged anvils

--- kubejs/server_scripts/repair_books.js
+++ kubejs/server_scripts/repair_books.js
@@ -1,38 +1,46 @@
-BlockEvents.rightClicked("minecraft:anvil", (event) => {
-  const player = event.player;
-  const player_name = player.name.string;
-  const coordinates = `${player.x} ${player.y} ${player.z}`;
-  const item = event.item;
-  const hand = event.hand;
-  const block = event.block;
-  const server = event.server;
+const anvils = [
+  "minecraft:anvil",
+  "minecraft:chipped_anvil",
+  "minecraft:damaged_anvil",
+];
 
-  if (!item || item.empty) return;
-  if (!(item.id === "immersiveenchanting:ancient_book")) return;
-  if (!player.isShiftKeyDown()) return;
-  if (hand == "OFF_HAND") return;
+for (const anvil of anvils) {
+  BlockEvents.rightClicked(anvil, (event) => {
+    const player = event.player;
+    const player_name = player.name.string;
+    const coordinates = `${player.x} ${player.y} ${player.z}`;
+    const item = event.item;
+    const hand = event.hand;
+    const block = event.block;
+    const server = event.server;
 
-  const nbt = item.toNBT()?.components;
+    if (!item || item.empty) return;
+    if (!(item.id === "immersiveenchanting:ancient_book")) return;
+    if (!player.isShiftKeyDown()) return;
+    if (hand == "OFF_HAND") return;
 
-  if ("minecraft:enchantments" in nbt) {
-    const enchantment = nbt["minecraft:enchantments"];
-    const x = block.x;
-    const y = block.y;
-    const z = block.z;
-    server.runCommandSilent(
-      `particle supplementaries:bottling_xp ${x} ${y} ${z} 0 0 0 .25 25`
-    );
-    server.runCommandSilent(
-      `playsound minecraft:item.wolf_armor.repair master ${player_name} ${coordinates}`
-    );
-    server.runCommandSilent(
-      `give ${player_name} immersiveenchanting:ancient_book[stored_enchantments=${enchantment}]`
-    );
-    player.swing();
-    if (!player.creativeMode) {
-      item.count--;
+    const nbt = item.toNBT()?.components;
+
+    if ("minecraft:enchantments" in nbt) {
+      const enchantment = nbt["minecraft:enchantments"];
+      const x = block.x;
+      const y = block.y;
+      const z = block.z;
+      server.runCommandSilent(
+        `particle supplementaries:bottling_xp ${x} ${y} ${z} 0 0 0 .25 25`,
+      );
+      server.runCommandSilent(
+        `playsound minecraft:item.wolf_armor.repair master ${player_name} ${coordinates}`,
+      );
+      server.runCommandSilent(
+        `give ${player_name} immersiveenchanting:ancient_book[stored_enchantments=${enchantment}]`,
+      );
+      player.swing();
+      if (!player.creativeMode) {
+        item.count--;
+      }
     }
-  }
 
-  event.cancel();
-});
+    event.cancel();
+  });
+}
