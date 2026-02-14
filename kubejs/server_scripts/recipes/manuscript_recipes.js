c37a1b5d85a504cdae34a8ccb286508d4981d733
phistoh <phis@mogobb.de>
2026-02-14 11:37:19 +0100

    Fixed manuscript transmutation

--- kubejs/server_scripts/recipes/manuscript_recipes.js
+++ kubejs/server_scripts/recipes/manuscript_recipes.js
@@ -1,15 +1,15 @@
 ServerEvents.recipes((event) => {
   const spell_schools = {
-    "irons_spellbooks:fire": "minecraft:blaze_rod",
-    "irons_spellbooks:ice": "irons_spellbooks:frozen_bone",
-    "irons_spellbooks:lightning": "irons_spellbooks:lightning_bottle",
-    "irons_spellbooks:holy": "irons_spellbooks:divine_pearl",
-    "irons_spellbooks:ender": "minecraft:ender_pearl",
-    "irons_spellbooks:blood": "irons_spellbooks:blood_vial",
-    "irons_spellbooks:evocation": "minecraft:emerald",
-    "irons_spellbooks:nature": "minecraft:poisonous_potato",
-    // "irons_spellbooks:eldritch": "minecraft:echo_shard",
-    "familiarslib:sound": "#c:music_discs",
+    "irons_spellbooks:fire": "kubejs:blazing_soul",
+    "irons_spellbooks:ice": "kubejs:freezing_soul",
+    "irons_spellbooks:lightning": "kubejs:crackling_soul",
+    "irons_spellbooks:holy": "kubejs:immaculate_soul",
+    "irons_spellbooks:ender": "kubejs:cosmic_soul",
+    "irons_spellbooks:blood": "kubejs:crimson_soul",
+    "irons_spellbooks:evocation": "kubejs:mystical_soul",
+    "irons_spellbooks:nature": "kubejs:wild_soul",
+    // "irons_spellbooks:eldritch": "kubejs:unfathomable_soul",
+    "familiarslib:sound": "kubejs:echoing_soul",
   };
   for (const [school, focus] of Object.entries(spell_schools)) {
     event
